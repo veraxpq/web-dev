0852d60937acfc616c693fecfd793a0a8babbb20
veraxpq <634505024@qq.com>
2021-10-21 19:26:41 -0700

    a6 2

--- src/components/a6/build/HomeScreen/HomeScreen.js
+++ src/components/a6/build/HomeScreen/HomeScreen.js
@@ -20,21 +20,4 @@ const HomeScreen = () => {
         </>
     );
 }
-
 export default HomeScreen;
-
-// (function () {
-//     ('#wd-homescreen').append(`
-//         <div class="row mt-2">
-//         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-//             ${NavigationSidebar()}
-//         </div>
-//         <div class="col-10 col-lg-7 col-xl-6 wd-postlist">
-//             ${PostList()}
-//         </div>
-//         <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-//             ${PostSummaryList()}
-//         </div>
-//         </div>
-//     `);
-// })();
